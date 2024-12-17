56f115883ee9a9704e313924dff8788422313878
Alee-za <leezey.x@gmail.com>
2024-12-17 16:46:29 +0500

    Files added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Mental Health Assistant',
+    version= '0.0.0',
+    author= 'Aliza Mustafa & Rahima Atiq '
+    author_email= 'bsai22108135@szabist.pk',
+    packages= find_packages(),
+    install_requires = []
+
+)
---
+++ src/prompt.py
@@ -0,0 +1,7 @@
+system_prompt = (
+    "You are an assistant specializing in mental health support. "
+    "Use the following pieces of retrieved context to provide helpful and empathetic responses. "
+    "If you don't know the answer, say that you don't know. Use three sentences maximum, ensure the tone is supportive, and keep the answer concise."
+    "\n\n"
+    "{context}"
+)
